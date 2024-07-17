2b308dfd4589f456829d58b24c019fd1cf145d63
Markus Hinsche <m.hinsche@gmail.com>
2024-07-17 13:58:50 +0200

    Clean App.js

--- src/App.js
+++ src/App.js
@@ -10,31 +10,6 @@ function App() {
       <header className="App-header">
         <WebView url="https://nailapp.theacare.de/nail-classifier" />
 
-        <Iframe
-          url="https://nailapp.theacare.de/nail-classifier"
-          width="640px"
-          height="320px"
-          id=""
-          className=""
-          display="block"
-          position="relative"
-        />
-        {/* <iframe
-          src="https://nailapp.theacare.de/nail-classifier"
-          // style="border:0px #ffffff none;"
-          name="myiFrame"
-          // scrolling="no"
-          frameBorder="1"
-          marginHeight="0px"
-          marginWidth="0px"
-          height="100%"
-          width="100%"
-          // allowfullscreen
-        ></iframe> */}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
