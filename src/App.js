10e683800743d5dfea205eadbb910715fc8880b7
Markus Hinsche <m.hinsche@gmail.com>
2024-07-17 14:17:05 +0200

    Use postMessage API, add onMessage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import Iframe from "react-iframe";
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import WebView from "./WebView";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <WebView url="https://nailapp.theacare.de/nail-classifier" />
+// const URL = "https://nailapp.theacare.de/nail-classifier";
+const URL = "http://localhost:3000/camera";
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const handleWebViewMessage = (message) => {
+    console.log("Received message from WebView:", message);
+  };
+
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <WebView url={URL} onMessage={handleWebViewMessage} />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
-}
+};
 
 export default App;
--- src/WebView.js
+++ src/WebView.js
@@ -1,4 +1,22 @@
-const WebView = ({ url }) => {
+import { useEffect, useRef, useState } from "react";
+
+const WebView = ({ url, onMessage }) => {
+  useEffect(() => {
+    const handleMessage = (event) => {
+      // Check if the message is from the expected origin
+      if (event.origin !== new URL(url).origin) {
+        return;
+      }
+      onMessage(event.data);
+    };
+
+    window.addEventListener("message", handleMessage);
+
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, [url, onMessage]);
+
   return (
     <iframe
       src={url}
